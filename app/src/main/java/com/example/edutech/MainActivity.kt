be8e5ab55d646fbe4c5a56a07efd86540ed3bb4c
Sheila Diaz Rojas <sheila.diaz@tecsup.edu.pe>
2026-04-26 15:34:53 -0500

    Conectar MainActivity con navegacion

--- app/src/main/java/com/example/edutech/MainActivity.kt
+++ app/src/main/java/com/example/edutech/MainActivity.kt
@@ -1,47 +1,27 @@
-package com.example.edutech
+package com.example.edutech // Paquete principal
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.edutech.ui.theme.EduTechAcademyTheme
+import com.example.edutech.navigation.AppNavigation // 👉 Importa navegación
+import com.example.edutech.ui.theme.EduTechAcademyTheme // Tema
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        enableEdgeToEdge() // 👉 Ajusta la app a toda la pantalla
+
         setContent {
             EduTechAcademyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+                AppNavigation()
+                // 👉 AQUÍ INICIA TODA LA APP
+                // 👉 NavController controla TODAS las pantallas
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EduTechAcademyTheme {
-        Greeting("Android")
     }
 }
