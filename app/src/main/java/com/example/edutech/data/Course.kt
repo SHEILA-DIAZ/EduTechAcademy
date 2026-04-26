e518f19c9c5fe79e3e9013980eb724f3d60bbebd
Sheila Diaz Rojas <sheila.diaz@tecsup.edu.pe>
2026-04-26 14:45:01 -0500

    Agregar modelo de cursos

---
+++ app/src/main/java/com/example/edutech/data/Course.kt
@@ -0,0 +1,21 @@
+package com.example.edutech.data // 👈 importante que esté en data
+
+data class Course( // 👈 data class (esto es clave)
+    val id: Int,
+    val title: String,
+    val instructor: String,
+    val level: String,
+    val category: String,
+    val duration: String,
+    val description: String,
+    val progress: Int = 0
+)
+
+val courseList = listOf(
+    Course(1, "Kotlin desde Cero", "Ana Torres", "Básico", "Programación", "6 horas", "Aprende Kotlin"),
+    Course(2, "Jetpack Compose", "Luis Pérez", "Intermedio", "Programación", "8 horas", "UI moderna"),
+    Course(3, "UI/UX", "María Gómez", "Básico", "Diseño", "5 horas", "Diseño apps"),
+    Course(4, "Figma", "Carlos Rojas", "Intermedio", "Diseño", "7 horas", "Prototipos"),
+    Course(5, "Marketing", "Sofía Vega", "Básico", "Negocios", "4 horas", "Marketing digital"),
+    Course(6, "Startup", "Pedro León", "Avanzado", "Negocios", "9 horas", "Emprendimiento")
+)
